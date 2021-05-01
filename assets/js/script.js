b8cee142402c27546221eec73350414335d7bd96
Shannon <shannon.nell92@gmail.com>
2021-05-01 16:56:40 -0400

    Pseudocode for  basic layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Click 'generate password' to prompt password criteria
   // - on click window prompt pops up
-    // - 
   // - password criteria
     // - prompt length of password between 8 - 128 characters
     // - prompt what character types: lowercase
@@ -9,9 +8,55 @@
       // - special characters
     // - when each prompt is answered, input should be validated and at least one char type selected
     // - when all prompts are answered, password is generated
-    // - when password is generated, it displays as alert or written on page
+    // - when password is generated, it displays as alert or written on page 
 
 
+/* ON GENERATE PASSWORD CLICK */
+// Window prompts asking
+  // - What length would you like your password to be (between 8 and 128 characters)?
+  // - Would you like to include uppercase letters? 
+  // - Would you like to include lowercase letters?
+  // - Would you like to inlude numeric values?
+  // - Would you like to include special characters (ex. !, *, $)?
+
+/* WHAT DO WE NEED TO CREATE A PASSWORD? */
+// VARIABLES
+// password length 8-128 chars
+
+// lowercase entries -- do I need to write the whole alphabet in lowercase vs uppercase?
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// uppercase entries
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+// numeric entries -- do these numbers need to be a string?
+var numeric = [1,2,3,4,5,6,7,8,9,0];
+
+// special characters -- do I include space OR / OR ' as a special char?
+var specialChars = ['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',':','<','=','>','?','@','[',']','^','_','`','{','}','|','~']
+
+
+// VALIDATE -- using if statements-- within a function?
+  // IF user inputs password length {length is true, so include in password};
+    // ELSE {prompt user that they need a password length of at least 8 characters, then run prompt again};
+
+  // IF user would like to include uppercase letters (var upperCase === true), {include upperCase options in password};
+    // ELSE {do not include upperCase options in password}
+
+  // IF user would like to include lowercase letters (var lowerCase === true), {include lowerCase options in password};
+    // ELSE {do not include lowerCase options in password}
+    // ELSE IF??
+
+  // IF user would like to include numeric values (var numeric === true), {include numeric options in password};
+    // ELSE {do not include numeric options in password}
+
+  // IF user would like to include special characters (var specialChars === true), {include special characters in password};
+    // ELSE {do not include special characters in password}
+
+
+
+
+/* JS for actual clicking and deploying of password */
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
