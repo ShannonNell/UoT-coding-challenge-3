1008b2823cc4c2e9e6de77f9b9da07aba29bf928
Shannon <shannon.nell92@gmail.com>
2021-05-02 16:10:30 -0400

    Can generate random character for each selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,21 @@
 // Click 'generate password' to prompt password criteria
-  // - on click window prompt pops up
-  // - password criteria
-    // - prompt length of password between 8 - 128 characters
-    // - prompt what character types: lowercase
-      // - uppercase
-      // - numeric
-      // - special characters
+  // - on start prompt pops up
+  // - √password criteria
+    // - √prompt length of password between 8 - 128 characters
+    // - √prompt what character types: lowercase
+      // - √uppercase
+      // - √numeric
+      // - √special characters
     // - when each prompt is answered, input should be validated and at least one char type selected
     // - when all prompts are answered, password is generated
     // - when password is generated, it displays as alert or written on page 
 
 
-/* ON GENERATE PASSWORD CLICK */
-// Window prompts asking
-  // - What length would you like your password to be (between 8 and 128 characters)?
-  // - Would you like to include uppercase letters? 
-  // - Would you like to include lowercase letters?
-  // - Would you like to inlude numeric values?
-  // - Would you like to include special characters (ex. !, *, $)?
-
-/* WHAT DO WE NEED TO CREATE A PASSWORD? */
 // VARIABLES
-// password length 8-128 chars
-
-// lowercase entries -- do I need to write the whole alphabet in lowercase vs uppercase?
+// Arrays of all possible selected inputs
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-// uppercase entries
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-// numeric entries -- do these numbers need to be a string?
 var numeric = [1,2,3,4,5,6,7,8,9,0];
-
-// special characters -- do I include space OR / OR ' as a special char?
 var specialChars = ['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',':','<','=','>','?','@','[',']','^','_','`','{','}','|','~']
 
 //FUNCITONS
@@ -92,13 +75,57 @@ function passwordQuestions() {
   } else {
     window.alert("You need to choose a number greater than 8 and less than 128.");
     passwordQuestions();  
-    return false;
   }
 }
-  
+
+// Need to generate random letter for lowerCase selection, UPPERcase selection, numeric, and special char that can be input into generatePassword eventuality
+// random letter lowercase
+function generateRandomLowerCase() {
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+console.log(generateRandomLowerCase());
+
+//random letter UPPER case
+function generateRandomUpperCase() {
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+console.log(generateRandomUpperCase());
+
+//random Numeric
+function generateRandomNumeric() {
+  return numeric[Math.floor(Math.random() * numeric.length)];
+}
+console.log(generateRandomNumeric());
+
+//random Special character
+function generateRandomSpecialChars() {
+  return specialChars[Math.floor(Math.random() * specialChars.length)];
+}
+console.log(generateRandomSpecialChars());
+
+
+
+
+/* random who knows what this is stuff:
+// once the prompts have been completed, need to be able to click the 'generate password' button and have it actually do something
+function generatePassword() {
+  // need the password length to be the number of characters outputted
+  console.log(passwordLength);
+}
+
+// for loop to loop through each letter of lowercase alphabet
+function randomNumber() {
+  for (var i = 0; i < lowerCase.length; i++) {
+    lowerCase = Math.floor(Math.random() * 27);
+    console.log(lowerCase[i]);
+  }
+}
+
+// RUN PASSWORD GENERATOR
 passwordQuestions();
 
-
+//Math.floor(Math.random() * 10);
+//  var lowerCase = lowerCase[i];
 
 
 /* JS for actual clicking and deploying of password */
